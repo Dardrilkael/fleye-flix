1d8171f477c76db2e1e3a450373e92bbc47e0de7
Dardrilkael <gabrielfonsecabarreto@gmail.com>
2024-10-03 00:53:17 -0300

    Added .env in frontend

--- frontend/src/api/Movies.js
+++ frontend/src/api/Movies.js
@@ -1,17 +1,16 @@
 import axios from "axios";
-
 const fetchMovies = (searchTerm) =>{
-    const result = axios.get(`http://192.168.0.223:8080/movies/${searchTerm}`);
+    const result = axios.get(`${process.env.REACT_APP_API_URL}/movies/${searchTerm}`);
     return result;
 }
 
 const fetchLikedMovies = () =>
-    axios.get("http://192.168.0.223:8080/liked-movies");
+    axios.get(`${process.env.REACT_APP_API_URL}/liked-movies`);
 
 const addFavoriteMovie = (movie) =>
-    axios.post("http://192.168.0.223:8080/liked-movies", movie);
+    axios.post(`${process.env.REACT_APP_API_URL}/liked-movies`, movie);
 
 const removeFavoriteMovie = (imdbID) =>
-    axios.delete(`http://192.168.0.223:8080/liked-movies/by-imdbid?imdbid=${imdbID}`);
+    axios.delete(`${process.env.REACT_APP_API_URL}/liked-movies/by-imdbid?imdbid=${imdbID}`);
 
 export{fetchMovies,fetchLikedMovies,addFavoriteMovie,removeFavoriteMovie}
